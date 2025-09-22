f91b8b17e4ad2d5bb4208833a90fb49ea778c1bc
godcodev <godcodevo@gmail.com>
2025-09-22 23:04:30 +0900

    fix(crawler): removed all comment

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,7 +1,3 @@
-# Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
-# SPDX-License-Identifier: MIT
-
-
 from .article import Article
 from .jina_client import JinaClient
 from .readability_extractor import ReadabilityExtractor
