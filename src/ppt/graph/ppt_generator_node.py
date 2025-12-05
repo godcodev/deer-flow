26556210463bcbb54e0aefc62d32a0f1d4cbc1e9
maanav1647 <asanohrsh@gmail.com>
2025-12-05 13:48:29 -0500

    refactor: optimize ppt_generator_node for cleaner and more maintainable code

--- src/ppt/graph/ppt_generator_node.py
+++ src/ppt/graph/ppt_generator_node.py
@@ -10,13 +10,20 @@ logger = logging.getLogger(__name__)
 
 def ppt_generator_node(state: PPTState):
     logger.info("Generating ppt file...")
-    # use marp cli to generate ppt file
-    # https://github.com/marp-team/marp-cli?tab=readme-ov-file
+
     generated_file_path = os.path.join(
         os.getcwd(), f"generated_ppt_{uuid.uuid4()}.pptx"
     )
-    subprocess.run(["marp", state["ppt_file_path"], "-o", generated_file_path])
-    # remove the temp file
+
+    # Run marp CLI to generate the ppt file
+    subprocess.run(
+        ["marp", state["ppt_file_path"], "-o", generated_file_path],
+        check=False
+    )
+
+    # Remove the temporary input file
     os.remove(state["ppt_file_path"])
+
     logger.info(f"generated_file_path: {generated_file_path}")
+
     return {"generated_file_path": generated_file_path}
